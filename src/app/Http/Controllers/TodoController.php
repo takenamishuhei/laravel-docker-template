fce2ca5006d644a03ca3813c215f4f4f590a171f
takenamishuhei <riaju.898246@gmail.com>
2024-05-10 19:05:27 +0900

    add: web.phpとindex.blade.phpに追記しルートを定義。create.blade.phpを作成しTodoController.phpに追記することで新規作成画面の表示

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -11,7 +11,12 @@ class TodoController extends Controller
     {
         $todo = new Todo();
         $todoList = $todo->all();
-        
+
         return view('todo.index', ['todoList' => $todoList]);
+    }
+
+    public function create()
+    {
+        return view('todo.create');
     }
 }
