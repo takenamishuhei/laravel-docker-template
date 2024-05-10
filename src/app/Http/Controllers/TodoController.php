a48b658ee522daf7cf491aff2219c0a7d44e7909
takenamishuhei <riaju.898246@gmail.com>
2024-05-10 18:58:04 +0900

    取得したデータを表示させるためにTodoController.phpとindex.blade.phpに追記

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -12,6 +12,6 @@ class TodoController extends Controller
         $todo = new Todo();
         $todoList = $todo->all();
         
-        return view('todo.index');
+        return view('todo.index', ['todoList' => $todoList]);
     }
 }
--- src/resources/views/todo/index.blade.php
+++ src/resources/views/todo/index.blade.php
@@ -12,33 +12,36 @@
 </head>
 <body>
   <div id="app">
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-      <div class="container">
-        <a class="navbar-brand" href="/todo">ToDo</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto"></ul>
-          <ul class="navbar-nav ml-auto"></ul>
+      <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+          <a class="navbar-brand" href="/todo">ToDo</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav ml-auto"></ul>
+          </div>
         </div>
-      </div>
-    </nav>
-    <main class="py-4">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-md-8">
-            <div class="card">
-              <div class="card-header">
-                ToDo一覧
-              </div>
-              <div class="list-group list-group-flush">
+      </nav>
+      <main class="py-4">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-md-8">
+              <p class="text-left">
+                <a class="btn btn-success" href="">ToDoを追加</a>
+              </p>
+              <div class="card">
+                <div class="card-header">
+                  ToDo一覧
+                </div>
+                <div class="list-group list-group-flush">
+                </div>
               </div>
             </div>
           </div>
-        </div>
-     </div>
-    </main>
-  </div>
-</body>
+      </div>
+      </main>
+    </div>
+  </body>
 </html>
