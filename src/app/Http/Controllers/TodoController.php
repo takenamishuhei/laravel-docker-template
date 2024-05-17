16c1b7401f423352639f6d97a617f35d40d953a3
takenamishuhei <riaju.898246@gmail.com>
2024-05-17 14:31:36 +0900

    add: コンストラクタインジェクションを実装してコントローラで使用するモデルインスタンスのリファクタリングを実行

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -8,10 +8,16 @@ use App\Todo;
 
 class TodoController extends Controller
 {
+    private $todo;
+
+    public function __construct(Todo $todo)
+    {
+        $this->todo = $todo;
+    }
+
     public function index()
     {
-        $todo = new Todo();
-        $todoList = $todo->all();
+        $todoList = $this->todo->all();
 
         return view('todo.index', ['todoList' => $todoList]);
     }
@@ -25,18 +31,17 @@ class TodoController extends Controller
     {
         $inputs = $request->all();
 
-        $todo = new Todo();
-        $todo->fill($inputs);
-        $todo->save();
+        $this->todo->fill($inputs);
+        $this->todo->save();
 
         return redirect()->route('todo.index');
     }
 
     public function show($id)
     {
-        $model = new Todo();
-        $todo = $model->find($id);
-
+        $todo = $this->todo->find($id);
+        
         return view('todo.show', ['todo' => $todo]);
     }
+
 }
