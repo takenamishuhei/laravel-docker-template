f697360afc6547f8c35951d3da66278d751c201a
takenamishuhei <riaju.898246@gmail.com>
2024-05-17 19:05:58 +0900

    feat: TodoRequest.phpを作成し、バリデーションを実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\TodoRequest;
 use App\Todo;
-use Symfony\Component\Console\Input\Input;
+use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
@@ -27,7 +27,7 @@ class TodoController extends Controller
         return view('todo.create');
     }
 
-    public function store(Request $request)
+    public function store(TodoRequest $request)
     {
         $inputs = $request->all();
 
@@ -51,7 +51,7 @@ class TodoController extends Controller
         return view('todo.edit', ['todo' => $todo]);
     }
 
-public function update(Request $request, $id)
+    public function update(TodoRequest $request, $id)
     {
         $inputs = $request->all();
         $todo = $this->todo->find($id);
